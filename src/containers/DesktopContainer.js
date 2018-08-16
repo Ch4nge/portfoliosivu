d2940d0d5067541f9db31b8f60639bb83713bf73
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-08-16 19:16:46 +0300

    Modify DesktopContainer added menu

--- src/containers/DesktopContainer.js
+++ src/containers/DesktopContainer.js
@@ -1,11 +1,34 @@
 import React from 'react'
-import { Responsive } from 'semantic-ui-react'
+import {
+  Responsive,
+  Menu,
+  Container,
+  Segment } from 'semantic-ui-react'
 
 class DesktopContainer extends React.Component {
   render() {
     return (
       <Responsive minWidth={Responsive.onlyTablet.minWidth}>
-
+        <Segment
+          inverted
+          textAlignt='center'
+          style={{ minHeight: 700, padding: '1em 0em' }}
+          vertical
+        >
+          <Menu size='large'>
+            <Container>
+              <Menu.Item>
+              Home
+              </Menu.Item>
+              <Menu.Item>
+              Blog
+              </Menu.Item>
+              <Menu.Item>
+              Coding activity
+              </Menu.Item>
+            </Container>
+          </Menu>
+        </Segment>
       </Responsive>
     )
   }
