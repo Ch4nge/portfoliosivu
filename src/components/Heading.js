be7ddef564235c9104ce5834a956181c9cdae3aa
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-08-16 22:47:36 +0300

    Add new component Skills

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import { Header, Icon, Button, Container } from 'semantic-ui-react'
 
 
 class Heading extends React.Component {
   render() {
     return (
-      <div>
-        Hello world!
-      </div>
+      <Container text textAlign='center'>
+        <Header
+          as='h1'
+          content='Sami Hautamäki'
+          inverted
+          style={{
+            fontSize: '4em',
+            fontWeight: 'normal',
+            marginBottom: 0,
+            marginTop: '3em' }} />
+        <Header
+          as='h2'
+          content='Your companys new programmer'
+          inverted
+          style={{
+            fontSize: '1.7em',
+            fontWeight: 'normal',
+            marginTop: '1.5em',
+          }}
+        />
+
+
+      </Container>
     )
   }
 }
---
+++ src/components/Skills.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Skills extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello world
+      </div>
+    )
+  }
+}
+
+export default Skills
