dde87982841837a2decc4e3ce0406815c8898151
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-08-16 22:55:11 +0300

    Modify App added Grid, and renders Skills

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import DesktopContainer from './containers/DesktopContainer'
+import Skills from './components/Skills'
+import { Grid } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
       <DesktopContainer>
-        Hello world!
+        <Grid>
+          <Skills />
+        </Grid>
       </DesktopContainer>
     )
   }
