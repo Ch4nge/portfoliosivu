b04cf5a149bb13fdfbd1602c052acf4fde4c6315
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-08-21 14:09:03 +0300

    Modify About more styles added

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header, Container, Grid } from 'semantic-ui-react'
+import { Segment, Image, Header, Container, Grid } from 'semantic-ui-react'
 
 class About extends React.Component {
   render() {
@@ -9,9 +9,12 @@ class About extends React.Component {
     const heaerStyle = {
       fontSize: '2em'
     }
+    const gridStyle = {
+      padding: '4em 0em'
+    }
     return (
-      <Grid padded='vertically' container stackable verticalAlign='middle'>
-        <Grid.Column width='12'>
+      <Grid style={gridStyle} padded='vertically' container stackable verticalAlign='middle'>
+        <Grid.Column width='8'>
           <Container>
             <Header as='h2'
               content='About this site'
@@ -19,6 +22,9 @@ class About extends React.Component {
             <p style={pStyle}> Imagine if you could create portfolio of sami hautamäki lightning fast.. </p>
             <p style={pStyle}> Well.. Now it is possible! endpoints are: </p>
           </Container>
+        </Grid.Column>
+        <Grid.Column width='6' floated='right'>
+          <Image bordered rounded size='small' src='http://16tiko1a.projects.tamk.fi/img/sami.png'/>
         </Grid.Column>
       </Grid>
     )
