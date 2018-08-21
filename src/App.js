11eecc723faddd4831b54a7050c7fb7c4e5bbf82
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-08-21 13:29:39 +0300

    Add new component About

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import DesktopContainer from './containers/DesktopContainer'
 import Skills from './components/Skills'
+import About from './components/About'
 
 class App extends Component {
   render() {
     return (
       <DesktopContainer>
+        <About />
         <Skills />
       </DesktopContainer>
     )
