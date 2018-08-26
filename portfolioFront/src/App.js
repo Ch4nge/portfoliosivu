11c6943fcaff15901d9e991822e577c807a2f6b0
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-08-26 19:23:32 +0300

    Add Admin new page

--- portfolioFront/src/App.js
+++ portfolioFront/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import DesktopContainer from './containers/DesktopContainer'
 import Home from './pages/Home'
+import Admin from './pages/Admin'
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 
 class App extends Component {
@@ -9,7 +10,7 @@ class App extends Component {
       <Router>
         <DesktopContainer>
           <Route exact path='/' render={() => <Home />} />
-          <Route path='/admin' render={() => <div> moi </div>} />
+          <Route path='/admin' render={() => <Admin />} />
         </DesktopContainer>
       </Router>
     )
---
+++ portfolioFront/src/pages/Admin.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+
+class Admin extends React.Component {
+  render() {
+    return (
+      <Grid padded='vertically' container stackable verticalAlign='middle'>
+        <Grid.Row>
+          <Grid.Cell>
+
+
+          </Grid.Cell>
+        </Grid.Row>
+      </Grid>
+    )
+  }
+}
+
+export default Admin
