150b945677be7f2b78990ee1a3f0463e37977686
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-08-26 19:37:29 +0300

    Modify Admin state changes with form

--- portfolioFront/src/pages/Admin.js
+++ portfolioFront/src/pages/Admin.js
@@ -2,13 +2,32 @@ import React from 'react'
 import { Form, Grid } from 'semantic-ui-react'
 
 class Admin extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      formName: 'skill'
+    }
+  }
+
+
+  onChangeHandler = (event) => {
+    this.setState({
+      [event.target.name]: [event.target.value][0]
+    })
+  }
   render() {
+    console.log(this.state.formName)
     return (
       <Grid padded='vertically' container stackable verticalAlign='middle'>
         <Grid.Row>
           <Grid.Column>
             <Form>
-              <Form.Field label='Form type' control='select'>
+              <Form.Field
+                onChange={this.onChangeHandler}
+                label='Form type'
+                control='select'
+                name='formName'>
                 <option value='skill'> Skill </option>
                 <option value='project'> Project </option>
               </Form.Field>
